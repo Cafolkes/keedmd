f073d93d59ff167cab6073abb29b96eb3c81fb17
Cafolkes <carl.axel.folkestad@gmail.com>
2020-02-15 16:31:27 -0800

    Added init files

--- core/systems/__init__.py
+++ core/systems/__init__.py
@@ -1,3 +1,4 @@
 from .double_inverted_pendulum import DoubleInvertedPendulum
 from .inverted_pendulum import InvertedPendulum
 from .cart_pole import CartPole
+from .closed_koopman_sys import ClosedSubspaceSys
