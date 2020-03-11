28432d7758120251aaa63d018c471ae7fb961d1b
Cafolkes <carl.axel.folkestad@gmail.com>
2020-03-10 20:31:10 -0700

    Bug fixes

--- core/learning/diffeomorphism_net.py
+++ core/learning/diffeomorphism_net.py
@@ -4,7 +4,7 @@ from torch import nn, cuda, optim, from_numpy, manual_seed, mean, transpose as t
 
 class DiffeomorphismNet(nn.Module):
 
-    def __init__(self, n, A_cl, jacobian_penalty = 1e-2, n_hidden_layers = 2, layer_width=50, batch_size = 64, dropout_prob=0.1):
+    def __init__(self, n, A_cl, jacobian_penalty = 1e-2, n_hidden_layers = 2, layer_width=50, batch_size = 64, dropout_prob=0.1, traj_input=False):
         super(DiffeomorphismNet, self).__init__()
         self.n = n
         self.n_hidden_layers = n_hidden_layers
@@ -12,21 +12,26 @@ class DiffeomorphismNet(nn.Module):
         self.batch_size = batch_size
         self.dropout_prob = dropout_prob
         self.jacobian_penalty = jacobian_penalty
+        self.traj_input = traj_input
 
-        N, d_h_in, H, d_h_out = batch_size, 2 * self.n, layer_width, self.n
+        N, H, d_h_out = batch_size, layer_width, self.n
+        if self.traj_input:
+            self.d_h_in = 2 * self.n
+        else:
+            self.d_h_in = self.n
 
         self.device = 'cuda' if cuda.is_available() else 'cpu'
         self.A_cl = A_cl.to(self.device)
-        self.fc_in = nn.Linear(d_h_in, H).double().to(self.device)
+        self.fc_in = nn.Linear(self.d_h_in, H).double().to(self.device)
         self.fc_hidden = []
         for _ in range(self.n_hidden_layers):
             self.fc_hidden.append(nn.Linear(H, H).double().to(self.device))
         self.fc_out = nn.Linear(H, d_h_out).double().to(self.device)
 
     def forward(self, x):
-        xt = x[:, :2 * self.n]  # [x, x_d]
-        xtdot = x[:, 2 * self.n:4 * self.n]  # [x_dot, xd_dot]
-        x_zero = cat((x[:, 4 * self.n:], x[:, self.n:2 * self.n]), 1)
+        xt = x[:, :self.d_h_in]  # [x]
+        xtdot = x[:, self.d_h_in:2 * self.d_h_in]  # [x_dot]
+        x_zero = x[:, 2 * self.d_h_in:]
         cur_batch_size = x.shape[0]
 
         # Define diffeomorphism model:
@@ -39,12 +44,12 @@ class DiffeomorphismNet(nn.Module):
         # Define diffeomorphism Jacobian model:
         h_grad = self.fc_in.weight
         h_grad = mm(self.fc_hidden[0].weight, h_grad)
-        h_grad = h_grad.unsqueeze(0).expand(cur_batch_size, self.layer_width, 2 * self.n)
-        delta = F.relu(self.fc_hidden[0](h[1])).sign().unsqueeze_(-1).expand(cur_batch_size, self.layer_width,2*self.n)
+        h_grad = h_grad.unsqueeze(0).expand(cur_batch_size, self.layer_width, self.d_h_in)
+        delta = F.relu(self.fc_hidden[0](h[1])).sign().unsqueeze_(-1).expand(cur_batch_size, self.layer_width,self.d_h_in)
         h_grad = delta * h_grad
         for ii in range(1, self.n_hidden_layers):
             h_grad = bmm(self.fc_hidden[ii].weight.unsqueeze(0).expand(cur_batch_size, self.layer_width, self.layer_width), h_grad)
-            delta = F.relu(self.fc_hidden[ii](h[ii+1])).sign().unsqueeze_(-1).expand(cur_batch_size,self.layer_width,2*self.n)
+            delta = F.relu(self.fc_hidden[ii](h[ii+1])).sign().unsqueeze_(-1).expand(cur_batch_size,self.layer_width,self.d_h_in)
             h_grad = delta * h_grad
 
         h_grad = bmm(self.fc_out.weight.unsqueeze(0).expand(cur_batch_size,self.n,self.layer_width), h_grad)
@@ -60,12 +65,12 @@ class DiffeomorphismNet(nn.Module):
 
         h_zerograd = self.fc_in.weight
         h_zerograd = mm(self.fc_hidden[0].weight, h_zerograd)
-        h_zerograd = h_zerograd.unsqueeze(0).expand(cur_batch_size, self.layer_width, 2 * self.n)
-        delta = F.relu(self.fc_hidden[0](h_zero[1])).sign().unsqueeze_(-1).expand(cur_batch_size, self.layer_width,2*self.n)
+        h_zerograd = h_zerograd.unsqueeze(0).expand(cur_batch_size, self.layer_width, self.d_h_in)
+        delta = F.relu(self.fc_hidden[0](h_zero[1])).sign().unsqueeze_(-1).expand(cur_batch_size, self.layer_width,self.d_h_in)
         h_zerograd = delta * h_zerograd
         for ii in range(1, self.n_hidden_layers):
             h_zerograd = bmm(self.fc_hidden[ii].weight.unsqueeze(0).expand(cur_batch_size, self.layer_width, self.layer_width), h_zerograd)
-            delta = F.relu(self.fc_hidden[ii](h_zero[ii+1])).sign().unsqueeze_(-1).expand(cur_batch_size, self.layer_width, 2*self.n)
+            delta = F.relu(self.fc_hidden[ii](h_zero[ii+1])).sign().unsqueeze_(-1).expand(cur_batch_size, self.layer_width, self.d_h_in)
             h_zerograd = delta * h_zerograd
         h_zerograd = bmm(self.fc_out.weight.unsqueeze(0).expand(cur_batch_size, self.n, self.layer_width), h_zerograd)
         h_zerograd = h_zerograd[:, :, :self.n]
